40704bfef2e94b19769e956cbfa313e3b1dd9ed0
epolat-github <etuna1999@gmail.com>
2020-07-23 02:23:23 +0300

    create "talepler" validation schema

---
+++ validationSchemas/talepSchema.js
@@ -0,0 +1,28 @@
+const Joi = require("@hapi/joi");
+
+const talepSchema = Joi.object({
+    TalepNo: Joi.number().required(),
+    Şirketİsmi: Joi.string().required(),
+    TalepDurumu: Joi.string().required(),
+    TalepTürü: Joi.string().required(),
+    TeslimTarihi: Joi.date().required(),
+    TaşımaTürü: Joi.string().valid("Yurtdışı", "Yurtiçi").required(),
+    İstasyonlar: Joi.object()
+        .keys({
+            Çıkış: Joi.string().required(),
+            Varış: Joi.string().required(),
+        })
+        .required(),
+    VagonBilgi: Joi.array().items(
+        Joi.object().keys({
+            KalemNo: Joi.number().required(),
+            VagonTipi: Joi.string().required(),
+            VagonSayısı: Joi.number().required(),
+            Eşyalar: Joi.array().items(Joi.string().required()).required(),
+            ToplamYük: Joi.number().required(),
+            YükBirimi: Joi.string().valid("ton", "kilo", "adet").required(),
+        })
+    ),
+});
+
+module.exports = talepSchema;
